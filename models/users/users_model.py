517bb1bb35e6fd85431125a40d565b107cce9799
Chocol4teChips <dragoonity987@gmail.com>
2022-05-03 16:40:22 +0700

    addUserModel

---
+++ models/users/users_model.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column
+from sqlalchemy.sql.sqltypes import Integer, String, DateTime, DECIMAL
+from sqlalchemy.sql import func
+
+from decimal import Decimal
+from models.database import Base
+from pydantic import BaseModel
+
+class dbUser(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    password = Column(String)
+    created_date = Column(DateTime(timezone=True), server_default=func.now())
+
+
+class userBase(BaseModel):
+    username: str
+    password: str
+
+
+class UserDisplayBase(BaseModel):
+    id: int
+    username: str
+
+    class config:
+        orm_mode = True
