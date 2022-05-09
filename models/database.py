a3cd09ad8588de6681c496e637cac0b4ac1c0971
Chocol4teChips <dragoonity987@gmail.com>
2022-05-09 15:06:11 +0700

    deploy db

--- models/database.py
+++ models/database.py
@@ -1,15 +1,29 @@
-from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from decouple import  config
+
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./.my-fast-api.db"
 
+if DEBUG:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./.my-fast-api.db"
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False}
+    )
+else:
+    DATABASE_USER = config("DATABASE_USER")
+    DATABASE_PASSWORD = config("DATABASE_PASSWORD")
+    DATABASE_NAME = config("DATABASE_NAME")
+    DATABASE_HOST = config("DATABASE_HOST")
+    SQLALCHEMY_DATABASE_URL = f"postgresql+phycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
 
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
